ff9b4d7d077f13c65f889a56fcdad5e3b9d3d17d
João Gabriel <joaogabriel9633@gmail.com>
2026-02-24 21:07:44 -0300

    chore: Set up development tooling, testing framework, and foundational UI components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <h2>Rocketseat</h2>
+      <Button>Click me</Button>
     </div>
   );
 }
