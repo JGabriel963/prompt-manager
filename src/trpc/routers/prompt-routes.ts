56cfb19faf2c769e0d444dca35778752395ee2bf
João Gabriel <joaogabriel9633@gmail.com>
2026-06-14 19:51:30 -0300

    Fix typecheck

--- src/trpc/routers/prompt-routes.ts
+++ src/trpc/routers/prompt-routes.ts
@@ -1,4 +1,4 @@
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { baseProcedure, createTRPCRouter } from '../init';
 
 export const promptRoutes = createTRPCRouter({
